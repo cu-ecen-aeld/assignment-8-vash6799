ab4467f847bcfb420b173c304635dcdc1dcae1bc
Varun Shah <vash6799@colorado.edu>
2026-03-01 11:01:25 -0700

    Pushed ldd.mk config and ldd modules

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -1,5 +1,5 @@
 #   git rev-parse HEAD
-LDD_VERSION = 6559cdaad15517e566c3af053c46b67493a8bc6d
+LDD_VERSION = 4cb92d06fad4b86466cf47235fe02c2f1f192cfa
 
 LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-vash6799.git
 LDD_SITE_METHOD = git
